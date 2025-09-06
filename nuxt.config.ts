fff4e3920cea5da44058ef45693f0c25f70ac9ea
jcjp <tchamver@hotmail.com>
2025-09-06 21:07:06 +0800

    feat: integrate nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxthub/core'],
   content: {
     preview: {
       dev: true,
