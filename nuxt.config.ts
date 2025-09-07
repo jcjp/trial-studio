154a36c104a1e8ca7769cdb60f15c07b0f3b6e0f
jcjp <tchamver@hotmail.com>
2025-09-07 17:16:00 +0800

    feat: add and setup nuxtui and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,28 @@
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxthub/core'],
+  modules: [
+    '@nuxt/content',
+    '@nuxthub/core',
+    '@nuxt/ui'
+  ],
+  css: [
+    '~/assets/css/main.css'
+  ],
   content: {
     preview: {
       dev: true,
       api: 'https://api.nuxt.studio',
     }
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+      tsconfigPaths(),
+    ]
   }
 })
